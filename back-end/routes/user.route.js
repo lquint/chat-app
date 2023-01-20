bc56e98b4761b87a6870c9a2cf0c61a868ab9d2e
lquint <83752971+lquint@users.noreply.github.com>
2023-01-20 14:05:30 +0100

    cors and cookie fix

--- back-end/routes/user.route.js
+++ back-end/routes/user.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bcrypt = require('mongoose-bcrypt');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
-
+const cookies = require("cookies");
 
 router.post('/signup', async (req, res) => {
   try {
@@ -18,17 +18,26 @@ router.post('/signup', async (req, res) => {
 router.post('/login', async (req, res) => {
     try {
         const { email, password } = req.body;
-        console.log(email,password)
         const user = await User.findByCredentials(email, password);
+        console.log(email, password)
         // creating jwt token
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-        res.status(200).json({
+        // setting jwt in a cookie
+        //cookie.set('jwt', token, { expires: 15 });
+        res.cookie("jwt", token, {
+          expires  : new Date(Date.now() + 9999999),
+          httpOnly: true,
+          sameSite: 'none'
+        })
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+        console.log(token)
+        res.status(200).send({
         token,
         user: {
             id: user._id,
             email: user.email,
         },
-        });
+        })
     } catch (error) {
         res.status(400).send(error);
     }
--- back-end/server.js
+++ back-end/server.js
@@ -3,14 +3,26 @@ const app = express();
 const mongoose = require('./config/db');
 const cors = require('cors');
 const userRoutes = require('./routes/user.route');
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+const corsOptions = {
+  credentials: true,
+  origin:'http://localhost:3000'
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
 // Use Routes
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.use('/api/user', userRoutes);
 
 const port = process.env.PORT || 5000;
